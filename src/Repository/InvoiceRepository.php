19aecfaf09287b4e54bb6f07b2dda0dd2fc4bb56
Jérémy Guillemot <dje430@gmail.com>
2019-08-25 09:28:46 +0200

    fix bug chrono for first invoice

--- src/Repository/InvoiceRepository.php
+++ src/Repository/InvoiceRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Invoice;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Exception;
 
 /**
  * @method Invoice|null find($id, $lockMode = null, $lockVersion = null)
@@ -28,43 +29,18 @@ class InvoiceRepository extends ServiceEntityRepository
      */
     public function findNextChrono(User $user)
     {
-        return $this->createQueryBuilder("i")
-                    ->select("i.chrono")
-                    ->join('i.customer', 'c')
-                    ->where("c.user = :user")
-                    ->setParameter('user', $user)
-                    ->orderBy('i.chrono', 'DESC')
-                    ->setMaxResults(1)
-                    ->getQuery()
-                    ->getSingleScalarResult() + 1;
+        try {
+            return $this->createQueryBuilder("i")
+                ->select("i.chrono")
+                ->join('i.customer', 'c')
+                ->where("c.user = :user")
+                ->setParameter('user', $user)
+                ->orderBy('i.chrono', 'DESC')
+                ->setMaxResults(1)
+                ->getQuery()
+                ->getSingleScalarResult() + 1;
+        } catch (\Exception $e) {
+            return 1;
+        }
     }
-
-    // /**
-    //  * @return Invoice[] Returns an array of Invoice objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('i.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Invoice
-    {
-        return $this->createQueryBuilder('i')
-            ->andWhere('i.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
